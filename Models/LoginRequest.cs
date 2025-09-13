d7cc22249724c37a74b5b91c926f6cbab94ed02f
dgaitan2025 <darwinnoez@gmail.com>
2025-09-12 22:58:55 -0600

    new

---
+++ Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ValidaNickname.Models
+{
+    public class LoginRequest
+    {
+        public string Usuario { get; set; } = string.Empty; // email o nickname
+        public string Password { get; set; } = string.Empty;
+    }
+}
